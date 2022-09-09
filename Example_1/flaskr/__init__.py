bacb41adffd677636f7d9e5ee23bf300b8a54a1f
Nyam <bkamunya77@gmail.com>
2022-09-09 17:50:05 +0300

    Added pagination

--- Example_1/flaskr/__init__.py
+++ Example_1/flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request 
 from models import setup_db, Plant
 from flask_cors import CORS
 
@@ -16,98 +16,18 @@ def create_app(test_config=None):
 
     @app.route('/plants')
     def get_plants():
+        page = request.args.get('page', 1, type=int)
+        start = (page-1) * 10
+        end = start + 10
+
         plants = Plant.query.all()
         formatted_plants = [plant.format() for plant in plants]
 
 
         return jsonify({
             "success": True,
-            "plants":formatted_plants,
-        })
-
-    @app.route('/')
-    def home():
-        data = [
-            {
-                "name": "Oleander",
-                "scientific_name": "Nerium oleander",
-                "is_poisonous": True,
-                "primary_color": "pink"
-            },
-
-            {
-                "name": "Water Hemlock",
-                "scientific_name": "Cicuta",
-                "is_poisonous": True,
-                "primary_color": "white"
-            },
-
-            {
-                "name": "Bamboo",
-                "scientific_name": "Bamboosa aridinarifolia",
-                "is_poisonous": False,
-                "primary_color": "green"
-            },
-
-            {
-                "name": "Carrot",
-                "scientific_name": "Daucas carota",
-                "is_poisonous": False,
-                "primary_color": "orange"
-            },
-
-            {
-                "name": "Lemon",
-                "scientific_name": "Citrus limonium",
-                "is_poisonous": False,
-                "primary_color": "yellow"
-            },
-             {
-                "name": "Foxglove",
-                "scientific_name": "Digitalis",
-                "is_poisonous": True,
-                "primary_color": "purple"
-            },
-             {
-                "name": "Lily of the Valley",
-                "scientific_name": "Convallaria majalis",
-                "is_poisonous": True,
-                "primary_color": "white"
-            },
-            {
-                "name": "Dieffenbachia",
-                "scientific_name": "Dieffenbachia seguine",
-                "is_poisonous": True,
-                "primary_color": "green"
-            },
-            {
-                "name": "Tomato",
-                "scientific_name": "Lycopersican esculentum",
-                "is_poisonous": False,
-                "primary_color": "red"
-            },
-             {
-                "name": "Spinach",
-                "scientific_name": "Lactuca sativa",
-                "is_poisonous": False,
-                "primary_color": "green"
-            },
-
-            {
-                "name": "Orange",
-                "scientific_name": "Citrus aurantium",
-                "is_poisonous": False,
-                "primary_color": "orange"
-            }
-        ]
-
-        for item in data:
-            plant = Plant(item['name'], item['scientific_name'], item['is_poisonous'], item['primary_color'])
-
-            plant.insert()
-
-        return jsonify({
-            "Success":True
+            "plants":formatted_plants[start:end],
+            "total_plants": len(formatted_plants)
         })
     
     return app
